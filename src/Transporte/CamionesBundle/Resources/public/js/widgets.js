fda55fa9ffe0e4c21bf69c6c151c2944bb96b01e
Diego Reyes <reyesdiego@hotmail.com>
2017-12-27 14:04:32 -0300

    testing messagebox

--- src/Transporte/CamionesBundle/Resources/public/js/widgets.js
+++ src/Transporte/CamionesBundle/Resources/public/js/widgets.js
@@ -628,9 +628,9 @@ var IngresoWidget = BaseWidget.extend({
           "Combinación no permitida",
           "El Movimiento Estacionamiento solo puede ser Terminal ZAP"
         );
-        $.msgBox({
-          "title": "<i class= 'fa fa-recycle txt-color-orangeDark'></i><span>Eliminar</span>",
-          "content": "<span>Desea Eliminar?</span>",
+        $.SmartMessageBox({
+          "title": "<div style='width: 50%'><i class= 'fa fa-recycle txt-color-orangeDark'></i><span>Eliminar</span></div>",
+          "content": "<div style='width: 50%'><span>¿Desea Eliminar?</span></div>",
           "buttons": "[No][Si]"
         }, function (r) {
           if (r === 'Si') {
